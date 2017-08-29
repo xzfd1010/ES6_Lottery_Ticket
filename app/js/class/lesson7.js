e736f57744c8ea18808d9602bffedd8ce4128d07
Nick <zhongcheng509@gmail.com>
2017-08-29 22:44:08 +0800

    函数扩展

---
+++ app/js/class/lesson7.js
@@ -0,0 +1,65 @@
+{
+    // 默认值后面不能再有没有默认值的变量
+    // 比如y后面不能有无默认值的c
+    function test(x, y = 'world') {
+        console.log('默认值', x, y)
+    }
+
+    test('hello');
+    test('hello', 'kill');
+
+}
+{
+    let x = 'test';
+
+    function test2(x, y = x) {
+        console.log('作用域', x, y);
+    }
+
+    test2('kill');//'kill'对应x，y=x现在自己的作用域中找x的值，再向上一级作用域查找
+}
+
+{
+    //rest参数， args后面不可以再有其他参数
+    function test3(...args) {
+        for (let v of args) {
+            console.log('rest', v);
+        }
+    }
+
+    test3(1, 2, 3, 4);
+}
+
+{
+    // 扩展运算符
+    console.log(...[1, 2, 3]);
+    console.log('a', ...[1, 2, 3]);
+}
+
+{
+    // 箭头函数，函数名，函数参数，函数返回值
+    let arrow = v => v * 2;
+    console.log('arrow', arrow(2));
+
+    let arrow2 = () => 5;
+    console.log(arrow2());
+
+    // 箭头函数的使用根据业务场景使用
+}
+
+{
+    // 尾调用，函数式编程，函数的最后一句话是不是一句函数？
+    function tail(x) {
+        console.log('tail', x);
+    }
+
+    function fx(x){
+        return tail(x);
+    }
+
+    fx(123);
+
+    // 尾调用的好处：提升性能，递归涉及到函数地址嵌套，相当耗费资源；
+
+
+}
