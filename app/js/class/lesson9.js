6161ac18929fc859d61dffa91ce1b020cac41e72
Nick <zhongcheng509@gmail.com>
2017-11-06 21:46:27 +0800

    Symbol

--- app/js/class/lesson9.js
+++ app/js/class/lesson9.js
@@ -1,25 +1,73 @@
+// 创建
+{
+    // 参数对象
+    const obj = {
+        toString() {
+            return 'abc';
+        }
+    };
+    const sym = Symbol(obj);
+    console.log(sym) // Symbol(abc)
+}
+// 转换
+{
+    let sym = Symbol('My symbol');
+    // console.log("your symbol is " + sym)
+    console.log("your symbol is " + sym.toString()) //只能显示转为字符串
+    console.log("Boolean:",Boolean(sym))
+}
+// 独一无二
 {
     // 声明,a1和a2不可能相等
     let a1 = Symbol();
     let a2 = Symbol();
     console.log(a1 === a2);
 
-    // a3是key值，查看此key是否在全局注册过，如果注册过就返回这个值，如果没有，就生成
-    let a3 = Symbol.for('a3');
-    let a4 = Symbol.for('a3');
-    console.log(a3 === a4);
+}
+// 作为对象属性
+{
+    let mySymbol = Symbol();
+    // 第一种写法
+    let a = {};
+    a[mySymbol] = 'Hello!';
+    // 第二种写法
+    let b = {
+        [mySymbol]: 'Hello!'
+    };
+    // 第三种写法
+    let c = {};
+    Object.defineProperty(c, mySymbol, { value: 'Hello!' });
+    // 以上写法都得到同样结果
+    console.log(a[mySymbol]) // "Hello!"
+    console.log(b[mySymbol]) // "Hello!"
+    console.log(c[mySymbol]) // "Hello!"
+}
+// 消除魔术字符串
+{
+    const shapeType = {
+        triangle: Symbol()
+    };
+    function getArea(shape, options) {
+        let area = 0;
+        switch (shape) {
+            case shapeType.triangle:
+                area = .5 * options.width * options.height;
+                break;
+        }
+        return area;
+    }
+    let area = getArea(shapeType.triangle, { width: 100, height: 100 });
+    console.log(area)
 }
 
+// 遍历
 {
-    // 使用场景
     let a1 = Symbol.for('abc');
     let obj = {
         [a1]:'123',
         'abc':345,
         'c':456
     };
-    console.log('obj', obj);
-
     // 如果有symbol做key值，通过for in 和let of都拿不到其属性
     for(let [key,value] of Object.entries(obj)){
         console.log('let of', key, value);
@@ -30,9 +78,48 @@
         console.log(obj[item]);
     });
 
-    // Reflect ownkeys，取到所有的key value值
+    // Reflect ownkeys，取到所有的key value值，直接调用forEach即可进行操作
     Reflect.ownKeys(obj).forEach(function (item) {
         console.log('ownkeys', item, obj[item]);
     });
+}
+
+// symbol.for
+{
+    // a3是key值，查看此key是否在全局注册过，如果注册过就返回这个值，如果没有，就生成
+    let a3 = Symbol.for('a3');
+    let a4 = Symbol.for('a3');
+    console.log(a3 === a4);
+
+    let a1 = Symbol("a1")
+    let a2 = Symbol.for("a1")
+    console.log(a1 === a2)
+}
+
+// symbol.keyFor
+{
+    let s1 = Symbol.for("foo");
+    console.log("s1",Symbol.keyFor(s1)) // "foo"
+    let s2 = Symbol("foo"); // 未登记
+    console.log("s2",Symbol.keyFor(s2)) // undefined
+}
+// hasInstance
+{
+    class MyClass {
+        [Symbol.hasInstance](foo) {
+            return foo instanceof Array;
+        }
+    }
+    console.log([1, 2, 3] instanceof new MyClass()) // true
+
+    // 自定义偶数类
+    class Even {
+        static [Symbol.hasInstance](obj) {
+            return Number(obj) % 2 === 0;
+        }
+    }
+    console.log(1 instanceof Even) // false
+    console.log(2 instanceof Even) // true
+    console.log(12345 instanceof Even) // false
 
 }
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -86,186 +86,179 @@ __webpack_require__(2);
 "use strict";
 
 
-// import { map, takeWhile, forEach } from "iterlib";
-{
-    // 默认值后面不能再有没有默认值的变量
-    // 比如y后面不能有无默认值的c
-    var test = function test(x) {
-        var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'world';
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-        console.log('默认值', x, y);
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// 创建
+{
+    // 参数对象
+    var obj = {
+        toString: function toString() {
+            return 'abc';
+        }
+    };
+    var sym = Symbol(obj);
+    console.log(sym); // Symbol(abc)
+}
+// 转换
+{
+    var _sym = Symbol('My symbol');
+    // console.log("your symbol is " + sym)
+    console.log("your symbol is " + _sym.toString()); //只能显示转为字符串
+    console.log("Boolean:", Boolean(_sym));
+}
+// 独一无二
+{
+    // 声明,a1和a2不可能相等
+    var a1 = Symbol();
+    var a2 = Symbol();
+    console.log(a1 === a2);
+}
+// 作为对象属性
+{
+    var mySymbol = Symbol();
+    // 第一种写法
+    var a = {};
+    a[mySymbol] = 'Hello!';
+    // 第二种写法
+    var b = _defineProperty({}, mySymbol, 'Hello!');
+    // 第三种写法
+    var c = {};
+    Object.defineProperty(c, mySymbol, { value: 'Hello!' });
+    // 以上写法都得到同样结果
+    console.log(a[mySymbol]); // "Hello!"
+    console.log(b[mySymbol]); // "Hello!"
+    console.log(c[mySymbol]); // "Hello!"
+}
+// 消除魔术字符串
+{
+    var getArea = function getArea(shape, options) {
+        var area = 0;
+        switch (shape) {
+            case shapeType.triangle:
+                area = .5 * options.width * options.height;
+                break;
+        }
+        return area;
     };
 
-    test('hello');
-    test('hello', 'kill');
+    var shapeType = {
+        triangle: Symbol()
+    };
+
+    var area = getArea(shapeType.triangle, { width: 100, height: 100 });
+    console.log(area);
 }
 
+// 遍历
 {
-    var test2 = function test2(x) {
-        var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : x;
+    var _obj2;
 
-        console.log('作用域', x, y);
-    };
+    var _a = Symbol.for('abc');
+    var _obj = (_obj2 = {}, _defineProperty(_obj2, _a, '123'), _defineProperty(_obj2, 'abc', 345), _defineProperty(_obj2, 'c', 456), _obj2);
+    // 如果有symbol做key值，通过for in 和let of都拿不到其属性
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-    var bar = function bar() {
-        var func = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function () {
-            return foo;
-        };
+    try {
+        for (var _iterator = Object.entries(_obj)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var _step$value = _slicedToArray(_step.value, 2),
+                key = _step$value[0],
+                value = _step$value[1];
 
-        var foo = 'inner';
-        console.log(func());
-    };
-
-    var x = 'test';
-
-    test2('kill'); //'kill'对应x，y=x现在自己的作用域中找x的值，再向上一级作用域查找
-
-    var foo = 'outer';
-
-    bar(); // outer
-}
-
-{
-    //rest参数， args后面不可以再有其他参数
-    var test3 = function test3() {
-        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-            args[_key] = arguments[_key];
+            console.log('let of', key, value);
         }
 
-        var _iteratorNormalCompletion = true;
-        var _didIteratorError = false;
-        var _iteratorError = undefined;
-
+        // 解决办法，获取用symbol作为key值的属性的对象
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
         try {
-            for (var _iterator = args[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                var v = _step.value;
-
-                console.log('rest', v);
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
             }
-        } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
         } finally {
-            try {
-                if (!_iteratorNormalCompletion && _iterator.return) {
-                    _iterator.return();
-                }
-            } finally {
-                if (_didIteratorError) {
-                    throw _iteratorError;
-                }
+            if (_didIteratorError) {
+                throw _iteratorError;
             }
         }
-    };
+    }
 
-    test3(1, 2, 3, 4);
+    Object.getOwnPropertySymbols(_obj).forEach(function (item) {
+        console.log(_obj[item]);
+    });
+
+    // Reflect ownkeys，取到所有的key value值，直接调用forEach即可进行操作
+    Reflect.ownKeys(_obj).forEach(function (item) {
+        console.log('ownkeys', item, _obj[item]);
+    });
 }
 
+// symbol.for
 {
-    var _console, _console2;
+    // a3是key值，查看此key是否在全局注册过，如果注册过就返回这个值，如果没有，就生成
+    var a3 = Symbol.for('a3');
+    var a4 = Symbol.for('a3');
+    console.log(a3 === a4);
 
-    // 扩展运算符
-    (_console = console).log.apply(_console, [1, 2, 3]);
-    (_console2 = console).log.apply(_console2, ['a'].concat([1, 2, 3]));
+    var _a2 = Symbol("a1");
+    var _a3 = Symbol.for("a1");
+    console.log(_a2 === _a3);
 }
 
+// symbol.keyFor
 {
-    // 箭头函数，函数名，函数参数，函数返回值
-    var arrow = function arrow(v) {
-        return v * 2;
-    };
-    console.log('arrow', arrow(2));
-
-    var arrow2 = function arrow2() {
-        return 5;
-    };
-    console.log(arrow2());
-
-    // 箭头函数的使用根据业务场景使用
+    var s1 = Symbol.for("foo");
+    console.log("s1", Symbol.keyFor(s1)); // "foo"
+    var s2 = Symbol("foo"); // 未登记
+    console.log("s2", Symbol.keyFor(s2)); // undefined
 }
-
+// hasInstance
 {
-    // 尾调用，函数式编程，函数的最后一句话是不是一句函数？
-    var tail = function tail(x) {
-        console.log('tail', x);
-    };
-
-    var fx = function fx(x) {
-        return tail(x);
-    };
-
-    fx(123);
-
-    // 尾调用的好处：提升性能，递归涉及到函数地址嵌套，相当耗费资源；
-}
-
-{
-    /**
-     * reduce
-     *    prev：前一个值
-     *    cur：当前值
-     *    index：索引
-     *    array：数组对象
-     */
-
-    var pipeline = function pipeline() {
-        for (var _len2 = arguments.length, funcs = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-            funcs[_key2] = arguments[_key2];
+    var MyClass = function () {
+        function MyClass() {
+            _classCallCheck(this, MyClass);
         }
 
-        return function (val) {
-            return funcs.reduce(function (a, b) {
-                return b(a);
-            }, val);
-        };
-    };
+        _createClass(MyClass, [{
+            key: Symbol.hasInstance,
+            value: function value(foo) {
+                return foo instanceof Array;
+            }
+        }]);
 
-    var pipeline2 = function pipeline2() {
-        for (var _len3 = arguments.length, funcs = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-            funcs[_key3] = arguments[_key3];
+        return MyClass;
+    }();
+
+    console.log([1, 2, 3] instanceof new MyClass()); // true
+
+    // 自定义偶数类
+
+    var Even = function () {
+        function Even() {
+            _classCallCheck(this, Even);
         }
 
-        return function (val) {
-            return funcs.reduce(function (a, b) {
-                return b(a);
-            }, val);
-        };
-    };
+        _createClass(Even, null, [{
+            key: Symbol.hasInstance,
+            value: function value(obj) {
+                return Number(obj) % 2 === 0;
+            }
+        }]);
 
-    var plus1 = function plus1(a) {
-        return a + 1;
-    };
-    var mult2 = function mult2(a) {
-        return a * 2;
-    };
-    var addThenMult = pipeline2(plus1, mult2);
+        return Even;
+    }();
 
-    console.log(addThenMult(5));
-}
-
-{
-    var m1 = function m1() {
-        var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
-            _ref$x = _ref.x,
-            x = _ref$x === undefined ? 0 : _ref$x,
-            _ref$y = _ref.y,
-            y = _ref$y === undefined ? 0 : _ref$y;
-
-        console.log([x, y]);
-    };
-
-    m1({ x: 0, y: undefined });
-}
-{
-    var m2 = function m2() {
-        var _ref2 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : { x: 0, y: 0 },
-            x = _ref2.x,
-            y = _ref2.y;
-
-        console.log([x, y]);
-    };
-
-    m2({ x: 0, y: undefined });
+    console.log(1 instanceof Even); // false
+    console.log(2 instanceof Even); // true
+    console.log(12345 instanceof Even); // false
 }
 
 /***/ })
